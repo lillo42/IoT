00972e792ae93224c6bd75ce77b1502413acd41e
Rafael Lillo <rafa_lillo@outlook.com>
2016-03-12 23:50:08 -0300

    Add commentery

--- Windows10/CryptoMessage/ServerCrypto/ServerCrypto/Cryptographic.cs
+++ Windows10/CryptoMessage/ServerCrypto/ServerCrypto/Cryptographic.cs
@@ -11,12 +11,20 @@ namespace ServerCrypto
         public static byte[] Encrypt(string text, string passPhrase)
         {
             IBuffer iv = null;
+
+            //Create SymmetricKeyAlgorithmProvider
             var symetric = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesCbcPkcs7);
 
+            //Create hash for passPhrase to create symmetric Key
             IBuffer keyBuffer = Hash(passPhrase);
+            //Create symmetric key
             CryptographicKey key = symetric.CreateSymmetricKey(keyBuffer);
 
+            //Convert texto to binary, for encrypt
             IBuffer data = CryptographicBuffer.ConvertStringToBinary(text, BinaryStringEncoding.Utf8);
+            
+            //Encrypt data
+            //Encrypt method return IBuffer
             return CryptographicEngine.Encrypt(key, data, iv).ToArray();
         }
 
@@ -24,21 +32,31 @@ namespace ServerCrypto
         {
             IBuffer iv = null;
 
+            //Create SymmetricKeyAlgorithmProvider
             var symetric = SymmetricKeyAlgorithmProvider.OpenAlgorithm(SymmetricAlgorithmNames.AesCbcPkcs7);
 
+            //Create hash for passPhrase to create symmetric Key
             IBuffer keyBuffer = Hash(passPhrase);
+            //Create symmetric key
             CryptographicKey key = symetric.CreateSymmetricKey(keyBuffer);
 
+            //Decrypt data
             IBuffer bufferDecrypted = CryptographicEngine.Decrypt(key, data, iv);
+            //Convert binary to string
             return CryptographicBuffer.ConvertBinaryToString(BinaryStringEncoding.Utf8, bufferDecrypted);
         }
 
         public static IBuffer Hash(string text)
         {
+            //Create HashAlgorithmProvider 
             var hash = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
+            //Create Hash object
             CryptographicHash cryptographicHash = hash.CreateHash();
+            //Convert string to binary
             IBuffer data = CryptographicBuffer.ConvertStringToBinary(text, BinaryStringEncoding.Utf8);
+            //Append data to generate Hash
             cryptographicHash.Append(data);
+            //Generate Hash
             return cryptographicHash.GetValueAndReset();
         }
     }
